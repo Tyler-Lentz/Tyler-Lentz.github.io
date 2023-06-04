e23c65a26d2291e507390310c46f625e950adeff
Tyler Lentz <tlentz@ucsd.edu>
2023-06-03 19:54:58 -0700

    improve gallery arrow styling

--- js/slide-gallery.js
+++ js/slide-gallery.js
@@ -5,21 +5,44 @@ class SlideGallery extends HTMLElement {
 
     connectedCallback() {
         this.attachShadow({mode: 'open'});
-        this.shadowRoot.innerHTML = this.#makeStyleHTML() + this.#makeSlotsHTML();
+        this.shadowRoot.innerHTML = this.#makeStyleHTML() + this.#makeSlotsHTML() + this.#makeButtonsHTML();
         this.#init();
     }
 
     #init() {
         this.current = 0;
+        this.leftButton = this.shadowRoot.getElementById("left-button");
+        this.rightButton = this.shadowRoot.getElementById("right-button");
+
         this.#setClasses();
+
+        // Set up keyboard navigation
         document.addEventListener('keydown', (e) => {
             if (e.key === "ArrowRight") {
                 e.preventDefault();
                 this.#scroll("right");
+                this.rightButton.classList.add("active");
             } else if (e.key === "ArrowLeft") {
                 e.preventDefault();
                 this.#scroll("left");
+                this.leftButton.classList.add("active");
             }
+        });
+
+        document.addEventListener('keyup', (e) => {
+            if (e.key === "ArrowRight") {
+                this.rightButton.classList.remove("active");
+            } else if (e.key === "ArrowLeft") {
+                this.leftButton.classList.remove("active");
+            }
+        });
+
+        // Set up button navigation
+        this.leftButton.addEventListener("click", () => {
+            this.#scroll("left");
+        });
+        this.rightButton.addEventListener("click", () => {
+            this.#scroll("right");
         });
     }
 
@@ -49,6 +72,16 @@ class SlideGallery extends HTMLElement {
                 elem.classList = "slide center";
             }
         }
+
+        if (this.current === 0) {
+            this.leftButton.setAttribute("disabled", "true");
+        } else if (this.current === this.children.length - 1) {
+            this.rightButton.setAttribute("disabled", "true");
+        }
+        else {
+            this.leftButton.removeAttribute("disabled");
+            this.rightButton.removeAttribute("disabled");
+        }
     }
 
     #makeStyleHTML() {
@@ -62,8 +95,7 @@ class SlideGallery extends HTMLElement {
 
                     border-left: var(--border);
                     border-right: var(--border);
-                    border-radius: 1em;
-                    padding: 0 0.5em;
+                    padding: 0 3em;
                 }
 
                 ::slotted(.slide) {
@@ -77,13 +109,82 @@ class SlideGallery extends HTMLElement {
                     grid-column: 1;
                 }
                 ::slotted(.left) {
-                    transform: translateX(-110%);
+                    transform: translateX(-115%);
                 }
                 ::slotted(.right) {
-                    transform: translateX(110%);
+                    transform: translateX(115%);
                 }
                 ::slotted(.center) {
                     transform: translateX(0);
+                }
+
+                .button-container {
+                    display: flex;
+                    margin: 0.5em auto;
+                    align-items: center;
+                    justify-content: center;
+
+                }
+
+                button {
+                    border: none;
+                    background: none;
+                    font-size: 2em;
+                    cursor: pointer;
+
+                    color: var(--ctp-frappe-overlay0);
+                    
+                    transition: opacity 0.5s ease-in-out;
+                    opacity: 1;
+                }
+
+                button:first-child {
+                    margin-right: 1em;
+                }
+
+                button:last-child {
+                    margin-left: 1em;
+                }
+
+                button:is(:hover,.active) {
+                    color: var(--ctp-frappe-mantle);
+
+                    background-color: var(--button-hover-bkg);
+                    border-radius: var(--button-border-radius);
+                }
+
+                button:is(:hover,.active) .arrow {
+                    border-color: var(--ctp-frappe-mantle);
+                }
+
+                button[disabled] {
+                    opacity: 0;
+                    cursor: default;
+                }
+
+                button[disabled] .arrow {
+                    opacity: 0;
+                    cursor: default;
+                }
+
+                /* Cool looking arrow for buttons from https://www.w3schools.com/howto/howto_css_arrows.asp */
+                .arrow {
+                    border: solid var(--ctp-frappe-overlay0);
+                    border-width: 0 .2em .2em 0;
+                    display: inline-block;
+                    padding: .2em;
+                    height: min-content;
+
+                    transition: opacity 0.5s ease-in-out;
+                    opacity: 1;
+                }
+
+                .arrow-right {
+                    transform: rotate(-45deg);
+                }
+
+                .arrow-left {
+                    transform: rotate(135deg);
                 }
             </style>
         `;
@@ -97,6 +198,16 @@ class SlideGallery extends HTMLElement {
         html += `</div>`;
         console.log(html)
         return html;
+    }
+
+    // controls for the gallery at the bottom
+    #makeButtonsHTML() {
+        return `
+            <div class="button-container">
+                <button id="left-button"> <span class="arrow arrow-left"></span>— </button>
+                <button id="right-button"> —<span class="arrow arrow-right"></> </button>
+            </div>
+        `;
     }
 }
 
