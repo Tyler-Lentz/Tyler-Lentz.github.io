6992fb0e7f62d266ceaeae926187c03bb3802a51
Tyler Lentz <tlentz@ucsd.edu>
2023-11-27 13:15:51 -0800

    basic post summary component working

--- js/post-summary.js
+++ js/post-summary.js
@@ -6,24 +6,52 @@ class PostSummary extends HTMLElement {
     connectedCallback() {
         this.attachShadow({mode: 'open'});
         this.shadowRoot.innerHTML = this.#makeStyleHTML() + this.#makeHTML();
-
-        const title = this.getAttribute("title");
-        const date = this.getAttribute("date");
-        const description = this.getAttribute("description");
-        const img = this.getAttribute('imgsrc');
     }
 
+    static get observedAttributes() {
+        return ["title", "date", "description", "imgsrc"];
+    }
+
+    attributeChangedCallback(name, _oldValue, newValue) {
+        if (name == "imgsrc") {
+            this.shadowRoot.getElementById("img").src = newValue;
+        } else {
+            this.shadowRoot.getElementById(name).innerText = newValue;
+        }
+    }
 
     #makeHTML() {
-        
+        return /*html*/`
+            <div id="container">
+                <img id="img" src="${this.getAttribute("imgsrc")}">
+                <p id="title"> ${this.getAttribute("title")} </p> 
+                <p id="date"> ${this.getAttribute("date")} </p>
+                <p id="description"> ${this.getAttribute("description")} </p>
+            </div>
+        `;
     }
 
     #makeStyleHTML() {
-        return `
+        return /*css*/`
             <style>
+                p {
+                    margin: 0;
+                    padding: 0;
+                }
+
+                #container {
+                    display: flex;
+                    flex-direction: column;
+                }
+
+                #date {
+                    font-size: 0.75em;
+                    margin-bottom: 1em;
+                }
+
             </style>
         `;
     }
 }
 
-customElements.define('project-summary', ProjectSummary);
+customElements.define('post-summary', PostSummary);
